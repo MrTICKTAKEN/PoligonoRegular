b76c2032d883ebfe6e72cff02edc08ea5a4ab547
MrTICKTAKEN <ticktaken25@gmail.com>
2024-02-14 22:09:57 -0600

    Agregar archivos de proyecto.

---
+++ PoligonoRegular/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PoligonoRegular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numLado = int.Parse(textBox1.Text);
+            double longitudLado = double.Parse(textBox2.Text);
+
+            double perimetro = CalcularPerimetro (numLado, longitudLado);
+
+            label1.Text = (" El perimetro del poligono regular con " + numLado + " lado y longitud de lado " + longitudLado + " es: " + perimetro);
+
+
+        }
+        private double CalcularPerimetro(int numLado, double longitudLado)
+        {
+            return numLado * longitudLado;
+            throw new NotImplementedException();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
